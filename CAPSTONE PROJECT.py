e215b4d50f95d11bf1316887df72a788b410b1a7
mehdi-bazzi <65427928+mehdi-bazzi@users.noreply.github.com>
2020-05-16 21:31:20 +0000

    NEW Ipython code

---
+++ CAPSTONE PROJECT.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# This notebook is used for the capstone projet. Thank you for your evaluation
+
+# In[3]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[4]:
+
+
+print ("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
